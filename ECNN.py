ced676b1a4c78ce344772374b057bf22f4f30252
Narasimha Murthy <jlnmurthy95@gmail.com>
2017-11-07 00:06:21 +0530

    Network is running

--- ECNN.py
+++ ECNN.py
@@ -1,14 +1,15 @@
 import numpy as np
+import os
 import keras
 from keras.models import Sequential
-from keras.layers import Conv2D, Activation, BatchNormalization
+from keras.layers import Conv2D, Activation, BatchNormalization, Input, Add
 from keras.models import Model
 from keras.optimizers import SGD, Adam, RMSprop
+import glob
+from classes import DataGenerator
 
 #creating a residual block
 def residual_block(x_input):
-    # x_input = Input(shape=(None, None, None, 64))
-
     conv1 = Conv2D(64, kernel_size=(3,3), strides=(1, 1), padding='same')(x_input)
     bn1 = BatchNormalization()(conv1)
     act1 = Activation('relu')(bn1)
@@ -16,13 +17,11 @@ def residual_block(x_input):
     conv2 = Conv2D(64, kernel_size=(3,3), strides=(1, 1), padding='same')(act1)
     bn2 = BatchNormalization()(conv2)
 
-    # model = Model(inputs=a, outputs=b)
-    # model.add()
-    return Add(axis=-1)([bn2, Activation('relu')(x_input), x_input])
+    return Add()([bn2, Activation('relu')(x_input), x_input])
 
 def build_ECNN():
 
-    x_input = Input(shape=(None, None, None, 4))
+    x_input = Input(shape=(None, None, 4))
 
     conv1 = Conv2D(64, kernel_size=(3,3), strides=(1, 1), padding='same')(x_input)
     bn1 = BatchNormalization()(conv1)
@@ -38,7 +37,7 @@ def build_ECNN():
     act3 = Activation('relu')(bn3)
 
     activation = act3
-    #attaching 13 residual blocks
+    # attaching 13 residual blocks
     for i in range(13):
         concat = residual_block(activation)
         activation = concat
@@ -57,10 +56,43 @@ def build_ECNN():
 
     return model
 
+def load_data(file_path):
+    with file_path.open() as file:
+        data = file.read_lines()
+    return data
+
+def read_paths(file_path):
+    with open(file_path, 'r') as data:
+        return data.readlines()
+
+#model summary
+model = build_ECNN()
+print model.summary()
+
+#model parameters
 max_iters = 40
 batch_size = 32
-
-
 sgd = SGD(lr=1e-2, decay=0.0005, momentum=0.9, nesterov=True)
 rmsprop = RMSprop(lr=1e-2, decay=0.0005, rho=0.9)
 adam = Adam(lr=1e-2, decay = 0.0005, beta_1 = 0.9, beta_2 = 0.999)
+
+#read paths of train, val and test images
+train_ids = read_paths('Data/train_imgs.txt')
+val_ids = read_paths('Data/val_imgs.txt')
+test_ids = read_paths('Data/test_imgs.txt')
+# os.chdir('Data/')
+model.compile(optimizer=sgd, loss='mse')
+
+params = {'dim_x': 224,
+          'dim_y': 224,
+          'dim_z': 4,
+          'batch_size': 32,
+          'shuffle': True}
+
+train_generator = DataGenerator(**params).generate(train_ids)
+val_generator = DataGenerator(**params).generate(val_ids)
+
+model.fit_generator(generator = train_generator,
+                    steps_per_epoch = len(train_ids)/batch_size,
+                    validation_data = val_generator,
+                    validation_steps = len(val_ids)/batch_size)
---
+++ classes.py
@@ -0,0 +1,95 @@
+import numpy as np
+import os
+from scipy import misc
+
+class DataGenerator(object):
+  'Generates data for Keras'
+  def __init__(self, dim_x = 32, dim_y = 32, dim_z = 32, batch_size = 32, shuffle = True):
+      'Initialization'
+      self.dim_x = dim_x
+      self.dim_y = dim_y
+      self.dim_z = dim_z
+      self.batch_size = batch_size
+      self.shuffle = shuffle
+
+  def generate(self, list_IDs):
+      'Generates batches of samples'
+      # Infinite loop
+      while 1:
+          # Generate order of exploration of dataset
+          indexes = self.__get_exploration_order(list_IDs)
+
+          # Generate batches
+          imax = int(len(indexes)/self.batch_size)
+          for i in range(imax):
+              # Find list of IDs
+              list_IDs_temp = [list_IDs[k] for k in indexes[i*self.batch_size:(i+1)*self.batch_size]]
+
+              # Generate data
+              X, y = self.__data_generation(list_IDs_temp)
+
+              yield X, y
+
+  def __get_exploration_order(self, list_IDs):
+      'Generates order of exploration'
+      # Find exploration order
+      indexes = np.arange(len(list_IDs))
+      if self.shuffle == True:
+          np.random.shuffle(indexes)
+
+      return indexes
+
+#computing the gradient map for combined images
+  def get_gradient(self, img):
+      i = np.zeros(img.shape)
+      i += np.array(img)
+
+      i_left = np.zeros(img.shape)
+      i_right = np.zeros(img.shape)
+      i_up = np.zeros(img.shape)
+      i_down = np.zeros(img.shape)
+
+      h = img.shape[0]
+      w = img.shape[1]
+
+      i_left[:,0:w-1,:] = i[:,1:w,:]
+      i_right[:,1:w,:] = i[:,0:w-1,:]
+      i_up[0:h-1,:,:] = i[1:h,:,:]
+      i_down[1:h,:,:] = i[0:h-1,:,:]
+
+      i_sum = abs(i-i_left) + abs(i-i_right) + abs(i-i_up) + abs(i-i_down)
+      i_sum = np.sum(i_sum,axis=2)
+      i_sum /= 4
+
+      return i_sum.reshape((i_sum.shape[0], i_sum.shape[1], 1))
+
+  def __data_generation(self, list_IDs_temp):
+      'Generates data of batch_size samples' # X : (n_samples, v_size, v_size, v_size, n_channels)
+      # Initialization
+      X = np.empty((self.batch_size, self.dim_x, self.dim_y, self.dim_z))
+      y = np.empty((self.batch_size, self.dim_x, self.dim_y, 1))
+
+      # Generate data
+      for i, ID in enumerate(list_IDs_temp):
+          # Store volume
+        #   X[i, :, :, :, 0] = np.load(ID + '.npy')
+
+
+          # Store class
+        #   y[i] = labels[ID]
+          ID = 'Data'+ ID.split('.')[1]
+          x_combined = misc.imread(str(ID)+'.jpg')
+          x_ref = misc.imread(str(ID)+'_r.jpg')
+          x_bg = misc.imread(str(ID)+'_b.jpg')
+          x_grad = self.get_gradient(x_combined)
+          x_ref_grad = self.get_gradient(x_ref)
+          x_bg_grad = self.get_gradient(x_bg)
+          print x_combined.shape
+          print x_grad.shape
+          x = np.concatenate((x_combined, x_grad), axis=2)
+
+          #can include gradients of reflection also here
+          X[i] = x
+          y[i] = x_bg_grad
+
+      return X, y
